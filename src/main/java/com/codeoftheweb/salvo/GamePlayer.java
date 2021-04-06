2884f70b887412dc21ee4af43f005d4a6dc235c6
FACULAPTOPHP\Facu <facundozumpano@gmail.com>
2021-04-06 16:22:38 -0300

    delete useless imports

--- src/main/java/com/codeoftheweb/salvo/GamePlayer.java
+++ src/main/java/com/codeoftheweb/salvo/GamePlayer.java
@@ -3,7 +3,6 @@ package com.codeoftheweb.salvo;
 import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -2,7 +2,6 @@ package com.codeoftheweb.salvo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
